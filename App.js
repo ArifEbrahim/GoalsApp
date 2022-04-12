fc88dca92a3d415bc4229984b9891cd24cb77803
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2022-04-12 11:33:28 +0100

    refactor: extracted GoalItem component

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import {
   StyleSheet,
-  Text,
   View,
   Button,
   TextInput,
   FlatList,
 } from "react-native";
+import GoalItem from "./components/GoalItem";
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -37,11 +37,7 @@ export default function App() {
         <FlatList
           data={courseGoals}
           renderItem={(itemData) => {
-            return (
-              <View style={styles.goalItem}>
-                <Text style={styles.goalText}>{itemData.item.text}</Text>
-              </View>
-            );
+            return <GoalItem text={itemData.item.text} />
           }}
           keyExtractor={(item, index) => item.id}
         />
@@ -74,14 +70,5 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
-  },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    backgroundColor: "#5e0acc",
-    borderRadius: 6,
-  },
-  goalText: {
-    color: "white",
-  },
+  }
 });
